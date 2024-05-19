00a990920f437913ce4171dc1cfa24867bdd1762
Jana Wolfová <janarega@seznam.cz>
2024-05-19 19:40:51 +0200

    hotovo

---
+++ src/main/java/cz/czechitas/java2webapps/lekce2/controller/DateTimeController.java
@@ -0,0 +1,28 @@
+package cz.czechitas.java2webapps.lekce2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+@Controller
+
+public class DateTimeController {
+
+    @GetMapping("/datum")
+    public ModelAndView datum() {
+        ModelAndView result  = new ModelAndView("datum");
+        result.addObject("datum", LocalDate.now());
+        return result;
+    }
+
+
+    @GetMapping("/cas")
+    public ModelAndView cas() {
+        ModelAndView result = new ModelAndView("cas");
+        result.addObject("cas", LocalTime.now());
+        return result;
+    }
+}
